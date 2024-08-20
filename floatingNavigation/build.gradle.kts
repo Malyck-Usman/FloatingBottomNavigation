038d8297b89cd7d60ff6bb11a070c0d67ab6ae75
MalikUsman <generalmail.6264@gmail.com>
2024-08-20 16:48:03 +0500

    Update

--- floatingNavigation/build.gradle.kts
+++ floatingNavigation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     id("maven-publish")
+    id("kotlin-kapt")
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
@@ -44,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.databinding:databinding-runtime:8.5.2")
 }
